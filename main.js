e1197e606b5fd9ed45d8e8518b40d8ad938b079d
tayeful912 <kazitayefulislam12@gmail.com>
2022-03-10 04:08:02 +0600

    invalid input bug fix

--- main.js
+++ main.js
@@ -3,28 +3,55 @@ document.getElementById('issueInputForm').addEventListener('submit', submitIssue
 
 
 function submitIssue(e) {
-  const getInputValue = id => document.getElementById(id).value;
+
+  //arrow funtion
+
+  let getInputValue = id => document.getElementById(id).value;
+
+
+
+
+
+
+
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
   const id = Math.floor(Math.random() * 100000000) + '';
   const status = 'Open';
 
+
   const issue = { id, description, severity, assignedTo, status };
 
   let issues = [];
 
 
-  if (localStorage.getItem('issues')) {
-    issues = JSON.parse(localStorage.getItem('issues'));
+  if (description == '' || assignedTo == '') {
+
+    return;
+
   }
-  issues.push(issue);
 
-  localStorage.setItem('issues', JSON.stringify(issues));
+  else {
 
-  document.getElementById('issueInputForm').reset();
-  fetchIssues();
-  e.preventDefault();
+    if (localStorage.getItem('issues')) {
+      issues = JSON.parse(localStorage.getItem('issues'));
+
+
+    }
+    issues.push(issue);
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    document.getElementById('issueInputForm').reset();
+    fetchIssues();
+    e.preventDefault();
+  }
+
+
+
+
+
 }
 
 const closeIssue = id => {
